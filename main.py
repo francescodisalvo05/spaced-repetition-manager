92705a295c7c1372b80b0ca72023ba4a55d8bae3
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-07-08 19:18:11 +0200

    Add chapter and num_extractions

---
+++ main.py
@@ -0,0 +1,41 @@
+import json
+import random
+
+from argparse import ArgumentParser
+
+
+def main():
+
+    parser = ArgumentParser()
+
+    parser.add_argument('-c', '--chapter', type=str, help='Select the chapter', required=True)
+    parser.add_argument('-n', '--num_extractions', type=int, default=1, help='Select the number of extractions')
+    # clean the session
+    # work with a default chapter -> take than a random one
+
+    parsed_args = parser.parse_args()
+
+    # read the index file
+    with open('index.json') as handle:
+        dict = json.loads(handle.read())
+
+    chapters = dict.keys()
+
+    chapter = parsed_args.chapter
+    num_extractions = parsed_args.num_extractions
+
+    # check if num_extractions is greater than 1
+    num_extractions = max(1,num_extractions)
+
+    if chapter in chapters:
+
+        print(f"Tell me from chapter {chapter}" + " >> ", end="")
+
+        for i in range(num_extractions):
+            print(random.randint(1,dict[chapter]), end=" ")
+    else:
+        print("Wrong chapter")
+
+
+if __name__ == '__main__':
+    main()
